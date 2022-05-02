9e86b1631c863da40aeaf1b440c3272b43ddbe4b
lafifii <fifii.geral@gmail.com>
2022-05-02 18:49:09 -0500

    'lolllll'

--- app.py
+++ app.py
@@ -10,9 +10,5 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     classes = request.args['classes']
-    version = request.args['version']
-    bucket_name = request.args['bucketname']
-    object_name = request.args['objectname']
-    file_name = request.args['filename']
 
     return 'hi'
